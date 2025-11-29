26b9e4bedc2c69b00968ae51785235d6033ee5a8
Andrej Bozic <abozic389@gmail.com>
2025-11-29 17:04:23 +0100

    feat: graph mixup initial

--- src/trainer.py
+++ src/trainer.py
@@ -1,29 +1,37 @@
-from functools import partial
+from typing import Callable
 
 import numpy as np
 import torch
+from torch.nn.modules.loss import MSELoss
+from torch.nn.modules.module import Module
+from torch.optim.lr_scheduler import LRScheduler
+from torch.optim.optimizer import Optimizer
 from tqdm import tqdm
+
+from logger import WandBLogger
+from models import GIN
+from qm9 import QM9DataModule
 
 
 class SemiSupervisedEnsemble:
     def __init__(
         self,
-        supervised_criterion,
-        optimizer,
-        scheduler,
-        device,
-        models,
-        logger,
-        datamodule,
+        supervised_criterion: torch.nn.MSELoss,
+        optimizer: Callable[..., torch.optim.Optimizer],
+        scheduler: Callable[..., torch.optim.lr_scheduler.LRScheduler],
+        device: torch.device,
+        models: list[torch.nn.Module],
+        logger: WandBLogger,
+        datamodule: QM9DataModule,
     ):
-        self.device = device
-        self.models = models
+        self.device: torch.device = device
+        self.models: list[Module] = models
 
         # Optim related things
-        self.supervised_criterion = supervised_criterion
+        self.supervised_criterion: MSELoss = supervised_criterion
         all_params = [p for m in self.models for p in m.parameters()]
-        self.optimizer = optimizer(params=all_params)
-        self.scheduler = scheduler(optimizer=self.optimizer)
+        self.optimizer: Optimizer = optimizer(params=all_params)
+        self.scheduler: LRScheduler = scheduler(optimizer=self.optimizer)
 
         # Dataloader setup
         self.train_dataloader = datamodule.train_dataloader()
@@ -38,22 +46,22 @@ class SemiSupervisedEnsemble:
             model.eval()
 
         val_losses = []
-        
+
         with torch.no_grad():
             for x, targets in self.val_dataloader:
                 x, targets = x.to(self.device), targets.to(self.device)
-                
+
                 # Ensemble prediction
                 preds = [model(x) for model in self.models]
                 avg_preds = torch.stack(preds).mean(0)
-                
+
                 val_loss = torch.nn.functional.mse_loss(avg_preds, targets)
                 val_losses.append(val_loss.item())
         val_loss = np.mean(val_losses)
         return {"val_MSE": val_loss}
 
     def train(self, total_epochs, validation_interval):
-        #self.logger.log_dict()
+        # self.logger.log_dict()
         results = []
         for epoch in (pbar := tqdm(range(1, total_epochs + 1))):
             for model in self.models:
@@ -63,11 +71,16 @@ class SemiSupervisedEnsemble:
                 x, targets = x.to(self.device), targets.to(self.device)
                 self.optimizer.zero_grad()
                 # Supervised loss
-                supervised_losses = [self.supervised_criterion(model(x), targets) for model in self.models]
-                supervised_loss = sum(supervised_losses)
-                supervised_losses_logged.append(supervised_loss.detach().item() / len(self.models))  # type: ignore
+                supervised_losses: list[torch.Tensor] = [
+                    self.supervised_criterion(model(x), targets)
+                    for model in self.models
+                ]
+                supervised_loss = torch.stack(supervised_losses).sum()
+                supervised_losses_logged.append(
+                    supervised_loss.detach().item() / len(self.models)
+                )
                 loss = supervised_loss
-                loss.backward()  # type: ignore
+                loss.backward()
                 self.optimizer.step()
             self.scheduler.step()
             supervised_losses_logged = np.mean(supervised_losses_logged)
@@ -83,4 +96,143 @@ class SemiSupervisedEnsemble:
                 summary_dict.update(val_metrics)
                 pbar.set_postfix(summary_dict)
             self.logger.log_dict(summary_dict, step=epoch)
+        return results
+
+
+class GraphMixup:
+    def __init__(
+        self,
+        supervised_criterion: torch.nn.MSELoss,
+        optimizer: Callable[..., torch.optim.Optimizer],
+        scheduler: Callable[..., torch.optim.lr_scheduler.LRScheduler],
+        device: torch.device,
+        models: list[torch.nn.Module],
+        logger: WandBLogger,
+        datamodule: QM9DataModule,
+        unsupervised_weight: float = 1.0,  # Max weight for unsupervised loss
+    ):
+        assert isinstance(models[0], GIN), "Only GIN is supported"
+        self.device = device
+        self.models = models
+        self.supervised_criterion = supervised_criterion
+
+        all_params = [p for m in self.models for p in m.parameters()]
+        self.optimizer = optimizer(params=all_params)
+        self.scheduler = scheduler(optimizer=self.optimizer)
+
+        self.train_dataloader = datamodule.train_dataloader()
+        # Use cycle to ensure we don't stop early if unlabeled data > labeled data
+        self.unsupervised_train_dataloader = datamodule.unsupervised_train_dataloader()
+
+        self.val_dataloader = datamodule.val_dataloader()
+        self.test_dataloader = datamodule.test_dataloader()
+        self.logger = logger
+        self.unsupervised_weight = unsupervised_weight
+
+    def validate(self):
+        for model in self.models:
+            model.eval()
+
+        val_losses = []
+        with torch.no_grad():
+            for batch in self.val_dataloader:
+                batch = batch.to(self.device)
+                targets = batch.y
+
+                # Ensemble prediction
+                preds = [model(batch) for model in self.models]
+                avg_preds = torch.stack(preds).mean(0)
+
+                val_loss = torch.nn.functional.mse_loss(avg_preds, targets)
+                val_losses.append(val_loss.item())
+        return {"val_MSE": np.mean(val_losses)}
+
+    def get_consistency_weight(self, epoch: int, total_epochs: int) -> float:
+        # Sigmoid ramp-up for unsupervised weight w(t) as per paper
+        # Increases from 0 to self.unsupervised_weight
+        rampup_length = total_epochs // 2
+        if epoch < rampup_length:
+            p = max(0.0, float(epoch)) / float(rampup_length)
+            p = 1.0 - p
+            return self.unsupervised_weight * np.exp(-5.0 * p * p)
+        else:
+            return self.unsupervised_weight
+
+    def train(self, total_epochs: int, validation_interval: int):
+        results = []
+
+        for epoch in (pbar := tqdm(range(1, total_epochs + 1))):
+            for model in self.models:
+                model.train()
+
+            epoch_loss_log = []
+
+            # w(t): Ramp-up weight for unsupervised loss
+            w_t = self.get_consistency_weight(epoch, total_epochs)
+
+            for labeled_batch, unlabeled_batch in zip(
+                self.train_dataloader, self.unsupervised_train_dataloader
+            ):
+                labeled_batch = labeled_batch.to(self.device)
+                unlabeled_batch = unlabeled_batch.to(self.device)
+
+                self.optimizer.zero_grad()
+                total_loss = 0.0
+
+                for model in self.models:
+                    # --- 1. GNN Forward Pass (Supervised) ---
+                    # Standard training on labeled data
+                    pred_gnn = model(labeled_batch)
+                    loss_gnn = self.supervised_criterion(pred_gnn, labeled_batch.y)
+
+                    # --- 2. Generate Pseudo-Labels (Unlabeled) ---
+                    # Use GNN to predict targets for unlabeled data
+                    with torch.no_grad():
+                        model.eval()
+                        pseudo_targets = model(unlabeled_batch)
+                        model.train()
+
+                    # --- 3. FCN Forward Pass (Supervised Mixup) ---
+                    # Manifold Mixup on labeled data
+                    # Model returns: pred, y_a, y_b, lam
+                    pred_sup, y_a, y_b, lam = model(
+                        labeled_batch, mixup=True, target=labeled_batch.y
+                    )
+
+                    # Mixup Loss calculation: lam * Loss(pred, y_a) + (1-lam) * Loss(pred, y_b)
+                    loss_fcn_sup = lam * self.supervised_criterion(pred_sup, y_a) + (
+                        1 - lam
+                    ) * self.supervised_criterion(pred_sup, y_b)
+
+                    # --- 4. FCN Forward Pass (Unsupervised Mixup) ---
+                    # Manifold Mixup on unlabeled data using Pseudo-Labels
+                    pred_unsup, y_a_u, y_b_u, lam_u = model(
+                        unlabeled_batch, mixup=True, target=pseudo_targets
+                    )
+
+                    loss_fcn_unsup = lam_u * self.supervised_criterion(
+                        pred_unsup, y_a_u
+                    ) + (1 - lam_u) * self.supervised_criterion(pred_unsup, y_b_u)
+
+                    # --- 5. Total Loss Combination ---
+                    # Loss = L_GNN + L_FCN_Supervised + w(t) * L_FCN_Unsupervised
+                    total_loss += loss_gnn + loss_fcn_sup + (w_t * loss_fcn_unsup)
+
+                loss: torch.Tensor = total_loss
+                loss.backward()
+                self.optimizer.step()
+
+                epoch_loss_log.append(loss.detach().item())
+
+            self.scheduler.step()
+            avg_loss = np.mean(epoch_loss_log)
+            results.append(avg_loss)
+
+            summary_dict = {"train_loss": avg_loss, "w_t": w_t}
+            if epoch % validation_interval == 0 or epoch == total_epochs:
+                val_metrics = self.validate()
+                summary_dict.update(val_metrics)
+                pbar.set_postfix(summary_dict)
+            self.logger.log_dict(summary_dict, step=epoch)
+
         return results
